f18be27e53b441f7b5be1f1cb4e783d910e1a74b
AntoninS <antonin.spontak@gmail.com>
2017-03-29 14:38:22 +0200

    Nearly fonctionnal display

--- script.js
+++ script.js
@@ -6,51 +6,68 @@ window.addEventListener("load", function() {
     sound.src = "test.mp3";
     sound.type = "audio/mpeg";
 
-
     var buttonLoadRSS = document.createElement("button");
     buttonLoadRSS.innerHTML = "Load RSS feed";
-    buttonLoadRSS.addEventListener("click", request)
-
+    buttonLoadRSS.addEventListener("click", getRSS)
 
     var inputRSS = document.createElement("input");
     inputRSS.id = "inputRSS";
+    inputRSS.value = "http://radiofrance-podcast.net/podcast09/rss_15644.xml" //With that we don't have to enter our url everytime
     elementPlayer.appendChild(buttonLoadRSS);
     elementPlayer.appendChild(inputRSS);
     elementPlayer.appendChild(sound);
 
-    function request() {
+    function getRSS() { // It could be nice to use a callback function, but it's bugging when I try to use it whith an event listener
         var xhr = new XMLHttpRequest();
-        var proxy = "https://crossorigin.me/"; //+ document.getElementById("inputRSS");
-        var url = proxy.concat(document.getElementById("inputRSS").value);
-        console.log(url);
+        var proxy = "https://crossorigin.me/"; // We need to use a proxy to fix the CORS problem
+        var url = proxy.concat(document.getElementById("inputRSS").value); //We add our proxy just before the RSS url
 
         xhr.open("GET", url, true);
-        //xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
-
-
         xhr.addEventListener('readystatechange', function() {
             if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status === 0)) {
-                alert(xhr.responseText);
+                displayListRSS(xhr);
             }
         });
         xhr.send(null);
     }
-/*
-    function readData(sData) {
 
-        var nodes = oData.getElementsByTagName("soft");
-        var ol = document.createElement("ol"),
-            li, cn;
+    function displayListRSS(rss) {
+        var doc = rss.responseXML;
+
+        var nodes = doc.getElementsByTagName("item");
+        console.log(nodes.length);
+
         for (var i = 0, c = nodes.length; i < c; i++) {
-            li = document.createElement("li");
-            cn = document.createTextNode(nodes[i].getAttribute("name"));
-            li.appendChild(cn);
-            ol.appendChild(li);
-        }
-        document.getElementById("podcastList").appendChild(ol);
+          var tr = document.createElement("tr");
+          var title = nodes[i].getElementsByTagName("title")[0].innerHTML;
+          var descr = nodes[i].getElementsByTagName("description")[0].innerHTML;
+          var podcastTable = document.getElementById('playlist-table');
+          var tdTitle = document.createElement("td");
+          tdTitle.innerHTML = title;
+          var tdDescription = document.createElement("td");
+          tdDescription.innerHTML = descr;
 
-        alert(sData);
+          podcastTable.appendChild(tr);
+          tr.appendChild(tdTitle);
+          tr.appendChild(tdDescription);
+        }
     }
-    */
+    /*
+        function readData(sData) {
+
+            var nodes = oData.getElementsByTagName("soft");
+            var ol = document.createElement("ol"),
+                li, cn;
+            for (var i = 0, c = nodes.length; i < c; i++) {
+                li = document.createElement("li");
+                cn = document.createTextNode(nodes[i].getAttribute("name"));
+                li.appendChild(cn);
+                ol.appendChild(li);
+            }
+            document.getElementById("podcastList").appendChild(ol);
+
+            alert(sData);
+        }
+        */
 
 });
